f11055e7d82475a1a4afb081b182ec81fe8439c9
wkhray <wkhray@gmail.com>
2024-10-31 14:20:17 +0800

    Update googlemap-test.py

--- Backend/google-map-api/googlemap-test.py
+++ Backend/google-map-api/googlemap-test.py
@@ -8,13 +8,40 @@ import os
 
 load_dotenv()
 
-def google_map_path(a, b):
+
+# find the location of a place
+def find_location(location: str, gmaps_client: googlemaps.Client)->str:
+    """
+    Find the location of a place
+    :param location: location input
+    :param gmaps_client: Google Map client
+    :return: a proper location name
+    """
+
+    # find a proper location name
+    res = gmaps.geocode(location)
+    print(res)
+    print(f"Location: {res[0]['formatted_address']}")
+    print(f"Latitude: {res[0]['geometry']['location']['lat']}")
+    print(f"Longitude: {res[0]['geometry']['location']['lng']}")
+    print("\n")
+
+    return res[0]['formatted_address']
+
+
+def google_map_path(a: str, b: str, gmaps: googlemaps.Client):
+    """
+    Get the path from point a to point b
+    :param a: Starting point
+    :param b: Destination point
+    :param gmaps: Google map client
+    :return: None
+    """
     # set up the google map API
-    gmaps = googlemaps.Client(key=os.getenv("GOOGLE_MAP_API_KEY"))
     print(f"Getting the path from {a} to {b}...")
 
     # get the direction from point a to point b with disabled friendly path
-    res = gmaps.directions(a, b, mode="walking", departure_time="now", alternatives=True)
+    res = gmaps.directions(a, b, mode="walking", departure_time="now", alternatives=True, language="zh-HK")
 
     print(res)
     # print the path in a readable format
@@ -36,10 +63,14 @@ def google_map_path(a, b):
 
         print("\n")
 
-# test the function
-google_map_path("Victoria Peak, Hong Kong", "Central, Hong Kong")
+def main():
+    google_map_client = googlemaps.Client(key=os.getenv("GOOGLE_MAP_API_KEY"))
+    # test the function
+    google_map_path("Victoria Peak, Hong Kong", "Central, Hong Kong", google_map_client)
 
-# TODO: find a way to change the input into google map readable format
-# a = input("Enter the starting point: ")
-# b = input("Enter the destination point: ")
-# google_map_path(a, b)
+    # TODO: find a way to change the input into google map readable format
+    # a = input("Enter the starting point: ")
+    # b = input("Enter the destination point: ")
+    # google_map_path(a, b)
+
+
