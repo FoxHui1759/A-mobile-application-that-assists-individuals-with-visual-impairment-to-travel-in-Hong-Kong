d22daf567a8f34ebfb560afab093fbb048bde4e2
FoxHui1759 <foxhui1759@gmail.com>
2024-11-29 20:49:00 +0800

    set up flutter

--- Backend/google-map-api/googlemap-test.py
+++ Backend/google-map-api/googlemap-test.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 
 # find the location of a place
-def find_location(location: str, gmaps_client: googlemaps.Client)->str:
+def find_location(location: str, gmaps_client: googlemaps.Client) -> str:
     """
     Find the location of a place
     :param location: location input
@@ -19,14 +19,14 @@ def find_location(location: str, gmaps_client: googlemaps.Client)->str:
     """
 
     # find a proper location name
-    res = gmaps.geocode(location)
+    res = googlemaps.geocode(location)
     print(res)
     print(f"Location: {res[0]['formatted_address']}")
     print(f"Latitude: {res[0]['geometry']['location']['lat']}")
     print(f"Longitude: {res[0]['geometry']['location']['lng']}")
     print("\n")
 
-    return res[0]['formatted_address']
+    return res[0]["formatted_address"]
 
 
 def google_map_path(a: str, b: str, gmaps: googlemaps.Client):
@@ -41,11 +41,13 @@ def google_map_path(a: str, b: str, gmaps: googlemaps.Client):
     print(f"Getting the path from {a} to {b}...")
 
     # get the direction from point a to point b with disabled friendly path
-    res = gmaps.directions(a, b, mode="walking", departure_time="now", alternatives=True, language="zh-HK")
+    res = gmaps.directions(
+        a, b, mode="walking", departure_time="now", alternatives=True, language="zh-HK"
+    )
 
     print(res)
     # print the path in a readable format
-    for i, step in enumerate(res[0]['legs'][0]['steps']):
+    for i, step in enumerate(res[0]["legs"][0]["steps"]):
         print(f"Step {i + 1}: {step['html_instructions']}")
         print(f"Distance: {step['distance']['text']}")
         print(f"Duration: {step['duration']['text']}")
@@ -53,18 +55,22 @@ def google_map_path(a: str, b: str, gmaps: googlemaps.Client):
         print(f"End location: {step['end_location']}")
 
         # also print the path in markdown format
-        instructions = step['html_instructions']
+        instructions = step["html_instructions"]
         instructions = instructions.replace("<b>", "**")
         instructions = instructions.replace("</b>", "**")
-        instructions = instructions.replace("<div style=\"font-size:0.9em\">", ", ")
+        instructions = instructions.replace('<div style="font-size:0.9em">', ", ")
         instructions = instructions.replace("</div>", "")
 
         print(f"Path: {instructions}")
 
         print("\n")
 
+
 def main():
-    google_map_client = googlemaps.Client(key=os.getenv("GOOGLE_MAP_API_KEY"))
+    google_map_client = googlemaps.Client(
+        key=os.getenv("AIzaSyB5W2CdEHJ3LTlIeSJz0uN8lIU2fZI7Nto")
+    )
+    print("Google Map API is set up")
     # test the function
     google_map_path("Victoria Peak, Hong Kong", "Central, Hong Kong", google_map_client)
 
@@ -74,3 +80,4 @@ def main():
     # google_map_path(a, b)
 
 
+main()
