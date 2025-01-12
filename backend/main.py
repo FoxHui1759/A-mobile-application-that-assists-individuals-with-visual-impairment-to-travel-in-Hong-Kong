d876da5b674ff4086f9650873cdf4ada3143e048
FoxHui1759 <foxhui1759@gmail.com>
2025-01-12 21:07:58 +0800

    flask server set up

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,15 @@
-# TO-DO python flask server
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+
+@socketio.on("connect")
+def handle_connect():
+    print("Client connected")
+    socketio.emit("message", "Hello from the server")
+
+
+if __name__ == "__main__":
+    socketio.run(app)
--- eyes_on_the_road/lib/pages/settings_page.dart
+++ eyes_on_the_road/lib/pages/settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -8,10 +9,40 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  String message = 'Watiting for message from the server...';
+  IO.Socket socket = IO.io('http://10.0.2.2:5000', <String, dynamic>{
+    'transports': ['websocket'],
+    'autoConnect': false,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Setting Page'),
+    socket.connect();
+
+    socket.onConnect((_) {
+      print('connect');
+    });
+
+    socket.on('message', (data) {
+      setState(() {
+        message = data;
+      });
+    });
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Settings Page',
+            style: TextStyle(fontSize: 20),
+          ),
+          Text(
+            message,
+            style: TextStyle(fontSize: 20),
+          )
+        ],
+      ),
     );
   }
 }
