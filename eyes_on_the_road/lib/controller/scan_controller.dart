7885a4850c46eff828256046d7c3cfc8d17d4701
FoxHui1759 <foxhui1759@gmail.com>
2025-04-11 21:56:56 +0800

    update gitignore and refacting

---
+++ eyes_on_the_road/lib/controller/scan_controller.dart
@@ -0,0 +1,24 @@
+import 'package:camera/camera.dart';
+import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class ScanController extends GetxController {
+  late CameraController cameraController;
+  late List<CameraDescription> cameras;
+
+  var isCameraReady = false.obs;
+
+  initCamera() async {
+    if (await Permission.camera.request().isGranted) {
+      cameras = await availableCameras();
+      cameraController = CameraController(
+        cameras[0],
+        ResolutionPreset.max,
+      );
+      await cameraController.initialize();
+      isCameraReady.value = true;
+    } else {
+      print("Permission denied");
+    }
+  }
+}
--- eyes_on_the_road/lib/views/camera_view.dart
+++ eyes_on_the_road/lib/views/camera_view.dart
@@ -1,8 +1,8 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'package:image/image.dart' as imglib;
+import 'package:get/get.dart';
+import 'package:eyes_on_the_road/controller/scan_controller.dart';
+import 'package:eyes_on_the_road/widgets/mic_popup.dart';
 
 class CameraView extends StatefulWidget {
   const CameraView({super.key, required this.camera});
@@ -20,44 +20,14 @@ class _CameraViewState extends State<CameraView> {
   String message = '';
 
   bool _inputting = false;
-  bool _serverReady = false;
-
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
 
   @override
   void initState() {
     super.initState();
-    //initialize the camera controller
-    _controller = CameraController(
-      widget.camera,
-      ResolutionPreset.medium,
-      imageFormatGroup: ImageFormatGroup.yuv420,
-      enableAudio: false,
-    );
-    _initializeControllerFuture = _controller.initialize().then((_) async {
-      if (!mounted) {
-        return;
-      }
-
-      setState(() {
-        message = 'start streaming';
-      });
-
-      await _controller.startImageStream((CameraImage cameraImage) {
-        //send the image to the server");
-        if (_serverReady) {
-          setState(() {
-            _serverReady = false;
-          });
-        }
-      });
-    });
   }
 
   @override
   void dispose() {
-    _controller.dispose();
     super.dispose();
   }
 
@@ -75,97 +45,31 @@ class _CameraViewState extends State<CameraView> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onLongPressStart: (details) {
-        _showMicrophone();
-      },
-      onLongPressEnd: (details) {
-        _hideMicrophone();
-      },
-      child: Container(
-        color: Theme.of(context).primaryColor,
-        child: Stack(
-          children: <Widget>[
-            /* Image(
-                fit: BoxFit.cover,
-                width: View.of(context).physicalSize.width,
-                height: View.of(context).physicalSize.height,
-                image: AssetImage('assets/images/street.jpg')), */
-            Center(
-              child: FutureBuilder<void>(
-                future: _initializeControllerFuture,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    return CameraPreview(_controller);
-                  } else {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                },
-              ),
-            ),
-            Container(
-              alignment: Alignment.center,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(top: 10.0),
-                    width: double.infinity,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.secondary,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Center(
-                      child: Text(nivagationCue,
-                          style: Theme.of(context).textTheme.bodyLarge),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(bottom: 10.0),
-                    width: double.infinity,
-                    height: 100,
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.secondary,
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Center(
-                      child: Text(distance,
-                          style: Theme.of(context).textTheme.bodyLarge),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            if (_inputting)
-              Container(
-                alignment: Alignment.center,
-                color: Colors.black.withOpacity(0.5),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      width: 300,
-                      height: 300,
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Icon(Icons.mic, size: 100, color: Colors.white),
-                          Text('Listening...',
-                              style: Theme.of(context).textTheme.bodyLarge),
-                          SizedBox(height: 20),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-          ],
-        ),
-      ),
+    return Scaffold(
+      body: GetBuilder<ScanController>(
+          init: ScanController(),
+          builder: (controller) {
+            return controller.isCameraReady.value
+                ? _LongPressDetector()
+                : const Center(
+                    child: Text("Loading Preview..."),
+                  );
+          }),
     );
+  }
+
+  Widget _LongPressDetector() {
+    return GestureDetector(
+        onLongPressStart: (details) {
+          _showMicrophone();
+        },
+        onLongPressEnd: (details) {
+          _hideMicrophone();
+        },
+        child: Stack(children: <Widget>[
+          if (_inputting)
+            MicPopup(), // Show the microphone popup when inputting
+          Container()
+        ]));
   }
 }
