9bb8ab9ff7efc462c75eef2dbd150d12276eb97c
FoxHui1759 <foxhui1759@gmail.com>
2025-04-17 20:10:05 +0800

    remove tflive-v2 and extra comment

--- eyes_on_the_road/lib/controller/scan_controller.dart
+++ eyes_on_the_road/lib/controller/scan_controller.dart
@@ -1,14 +1,13 @@
 import 'package:camera/camera.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:tflite_v2/tflite_v2.dart';
 
 class ScanController extends GetxController {
   @override
   void onInit() {
     super.onInit();
     initCamera();
-    initTFLite();
+    //initTFLite();
   }
 
   @override
@@ -50,33 +49,7 @@ class ScanController extends GetxController {
     }
   }
 
-  initTFLite() async {
-    await Tflite.loadModel(
-        model: "assets/model.tflite",
-        labels: "assets/labels.txt",
-        isAsset: true,
-        numThreads: 1,
-        useGpuDelegate: false);
-  }
+  initTFLite() async {}
 
-  objectDetector(CameraImage image) async {
-    var detector = await Tflite.runModelOnFrame(
-      bytesList: image.planes.map((e) => e.bytes).toList(),
-      asynch: true,
-      imageHeight: image.height,
-      imageWidth: image.width,
-      imageMean: 127.5,
-      imageStd: 127.5,
-      numResults: 1,
-      rotation: 90,
-      threshold: 0.4,
-    );
-
-    if (detector != null) {
-      print("Detected: $detector");
-      // Process the detected objects
-    } else {
-      print("No objects detected");
-    }
-  }
+  objectDetector(CameraImage image) async {}
 }
