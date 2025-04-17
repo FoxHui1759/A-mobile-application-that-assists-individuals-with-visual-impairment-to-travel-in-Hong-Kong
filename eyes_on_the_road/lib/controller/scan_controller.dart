11535ea8000201aea8c20ccbb11793e411f0ba88
FoxHui1759 <foxhui1759@gmail.com>
2025-04-17 15:19:23 +0800

    add TFlite component in scan controller

--- eyes_on_the_road/lib/controller/scan_controller.dart
+++ eyes_on_the_road/lib/controller/scan_controller.dart
@@ -1,12 +1,14 @@
 import 'package:camera/camera.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:tflite_v2/tflite_v2.dart';
 
 class ScanController extends GetxController {
   @override
   void onInit() {
     super.onInit();
     initCamera();
+    initTFLite();
   }
 
   @override
@@ -18,20 +20,63 @@ class ScanController extends GetxController {
   late CameraController cameraController;
   late List<CameraDescription> cameras;
 
+  var frameCount = 0;
+
   var isCameraReady = false.obs;
 
   initCamera() async {
     if (await Permission.camera.request().isGranted) {
       cameras = await availableCameras();
+
       cameraController = CameraController(
         cameras[0],
         ResolutionPreset.max,
+        enableAudio: false,
+        imageFormatGroup: ImageFormatGroup.yuv420,
       );
-      await cameraController.initialize();
+      await cameraController.initialize().then((value) {
+        cameraController.startImageStream((CameraImage image) {
+          frameCount++;
+          if (frameCount % 10 == 0) {
+            objectDetector(image);
+          }
+          update();
+        });
+      });
       isCameraReady(true);
       update();
     } else {
       print("Permission denied");
+    }
+  }
+
+  initTFLite() async {
+    await Tflite.loadModel(
+        model: "assets/model.tflite",
+        labels: "assets/labels.txt",
+        isAsset: true,
+        numThreads: 1,
+        useGpuDelegate: false);
+  }
+
+  objectDetector(CameraImage image) async {
+    var detector = await Tflite.runModelOnFrame(
+      bytesList: image.planes.map((e) => e.bytes).toList(),
+      asynch: true,
+      imageHeight: image.height,
+      imageWidth: image.width,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      numResults: 1,
+      rotation: 90,
+      threshold: 0.4,
+    );
+
+    if (detector != null) {
+      print("Detected: $detector");
+      // Process the detected objects
+    } else {
+      print("No objects detected");
     }
   }
 }
